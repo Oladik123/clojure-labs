b64261065b83310afffc8eb65a09fa0e8dbe502e
Vitchenko Vladislav <v.vitchenko@softaria.com>
2020-11-06 15:26:08 +0700

    lab 2-0 wip

--- src/clojure_labs/core.clj
+++ src/clojure_labs/core.clj
@@ -1,47 +1,24 @@
 (ns clojure-labs.core
   (:use [clojure-labs.utils]))
 
-(defn extended-by-letter-word
-  [word]
-  (fn [letter]
-    (cons (first letter) word))
-  )
 
-(defn collection-of-extended-by-letters-word
-  [letters]
-  (fn [word]
-    (map (extended-by-letter-word word) letters))
-  )
-
-(defn filtered-collection
-  [predicate collection-supplier]
-  (fn [& args]
-    (filter predicate (apply collection-supplier args)))
-  )
-
-(defn words-set
-  [letters length]
-  (map
-    reverse
-    (loop [current-length 1
-           words letters]
-      (if (< current-length length)
-        (recur (inc current-length)
-               (apply
-                 concat
-                 (map
-                   (filtered-collection
-                     (fn [word] (not= (first word) (first (rest word))))
-                     (collection-of-extended-by-letters-word letters)
-                     )
-                   words
-                   )
-                 ))
-        words
-        )
+(defn integral
+  [function iterations]
+  (fn [argument]
+    (let [step (/ argument iterations)]
+      (* step
+         (+ (/ (+ (function 0) (function argument)) 2)
+            (apply + (map (fn [x] (function x)) (range 0 argument step)))
+            )
+         )
       )
     )
   )
 
-(println "result")
-(println (words-set (list "a" "b" "c") 3))
+
+(defn function
+  [x]
+  (* (- x (/ x 2)) (- x (/ x 2)))
+  )
+
+(println ((integral function 1000) 100000))
