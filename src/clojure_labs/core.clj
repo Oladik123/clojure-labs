f06891891025eec7c59409af34cd0455695e87a6
Vladislav <v.vitchenko@g.nsu.ru>
2020-12-19 00:10:35 +0700

    lab 3-2

--- src/clojure_labs/core.clj
+++ src/clojure_labs/core.clj
@@ -1,32 +1,28 @@
-(ns clojure-labs.core (:use [clojure-labs.utils]))
+(ns clojure-labs.core
+  (:use [clojure-labs.utils]))
 
-(defn break-coll
+(defn split
   [part-size coll]
   (take-while #(not (empty? %))
               (lazy-seq
                 (cons
                   (take part-size coll)
-                  (break-coll part-size (drop part-size coll))))))
+                  (split part-size (drop part-size coll))))))
 
-(defn parallel-filter
-  [predicate part-size coll]
-  (->> coll
-       (break-coll part-size)
-       (map #(future (doall (filter predicate %))))
-       (doall)
-       (map deref)
+(defn heavy-even? [x] (do (Thread/sleep 10) true))
+
+(defn lazy-parallel-filter [sublist-size thread-count predicate coll]
+  (->> (split (* sublist-size thread-count) coll)
+       (map (fn [list] (->> (split sublist-size list)
+                            (map #(future (do (println "running") (doall (filter predicate %)))))
+                            ;(map #(future (doall (filter predicate %))))
+                            (doall)
+                            (map deref)
+                            (doall))))
+       (apply concat)
        (apply concat)))
 
-(defn parallel-filter-by-threads
-  [threads predicate coll]
-  (parallel-filter predicate (/ (count coll) threads) coll))
+(def configured-lazy-filter (partial lazy-parallel-filter 100 4))
 
-(def naturals (take 100 (iterate inc 0)))
-(def naturals2 (take 100 (iterate inc 0)))
-
-(defn heavy-even? [x] (do (Thread/sleep 100) (even? x)))
-
-(def configured-parallel-filter (partial parallel-filter-by-threads 4))
-
-(time (doall (filter heavy-even? naturals)))
-(time (configured-parallel-filter heavy-even? naturals2))
+(time (doall (take 1000 (configured-lazy-filter heavy-even? (iterate inc -10)))))
+(time (doall (take 1000 (filter heavy-even? (iterate inc 0)))))
