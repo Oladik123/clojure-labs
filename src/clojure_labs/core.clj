24fa377570383d371dd85e50083b7fbfa9aaf643
Vladislav <v.vitchenko@g.nsu.ru>
2020-12-19 00:13:42 +0700

    lab 3-1

--- src/clojure_labs/core.clj
+++ src/clojure_labs/core.clj
@@ -1,17 +1,17 @@
 (ns clojure-labs.core (:use [clojure-labs.utils]))
 
-(defn break-coll
+(defn split
   [part-size coll]
   (take-while #(not (empty? %))
               (lazy-seq
                 (cons
                   (take part-size coll)
-                  (break-coll part-size (drop part-size coll))))))
+                  (split part-size (drop part-size coll))))))
 
 (defn parallel-filter
   [predicate part-size coll]
   (->> coll
-       (break-coll part-size)
+       (split part-size)
        (map #(future (doall (filter predicate %))))
        (doall)
        (map deref)
@@ -22,11 +22,10 @@
   (parallel-filter predicate (/ (count coll) threads) coll))
 
 (def naturals (take 100 (iterate inc 0)))
-(def naturals2 (take 100 (iterate inc 0)))
 
 (defn heavy-even? [x] (do (Thread/sleep 100) (even? x)))
 
-(def configured-parallel-filter (partial parallel-filter-by-threads 4))
+(def configured-parallel-filter (partial parallel-filter-by-threads 1))
 
 (time (doall (filter heavy-even? naturals)))
-(time (configured-parallel-filter heavy-even? naturals2))
+(time (configured-parallel-filter heavy-even? naturals))
